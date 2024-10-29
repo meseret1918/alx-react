1714450cf6c7a22f60ce260be0e3804453e0e8cd
meseret1918 <meseret1918@gmail.com>
2024-10-30 01:00:44 +0300

    task2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,32 +1,23 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'images/',
-            },
-          },
-        ],
-      },
-    ],
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
   },
+  mode: 'production'
 };
 

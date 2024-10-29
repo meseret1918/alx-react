3df689b8f653758aafb24e6a06a3d96c4ef3724f
meseret1918 <meseret1918@gmail.com>
2024-10-30 00:46:36 +0300

    task2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,10 +1,7 @@
 // js/dashboard_main.js
 import $ from 'jquery';
 import _ from 'lodash';
-import '../css/main.css';
-import logo from '../assets/holberton-logo.jpg';
 
-$('body').append(`<div id="logo" style="background-image: url(${logo}); width: 200px; height: 200px;"></div>`);
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');

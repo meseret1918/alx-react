b20657757331ae46d3077369ba32d3e077b40fe5
meseret1918 <meseret1918@gmail.com>
2024-10-29 12:01:33 +0000

    Finalize Webpack setup and verify functionality

---
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -0,0 +1,15 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
+

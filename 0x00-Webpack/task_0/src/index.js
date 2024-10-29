86b67e7edef314da73de2d1e9bbcd38987be3364
meseret1918 <meseret1918@gmail.com>
2024-10-29 11:33:13 +0000

    Add jQuery script to dynamically append content

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,9 @@
 import $ from 'jquery';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(() => {
+  // Append three paragraphs to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
 

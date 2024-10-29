54c75d3022afec0b97b73f93082bf219cb250e70
meseret1918 <meseret1918@gmail.com>
2024-10-29 12:38:00 +0000

    delete

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(() => {
-  // Append three paragraphs to the body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
-

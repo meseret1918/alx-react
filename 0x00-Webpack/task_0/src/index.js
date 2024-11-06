98dd6aa3ded1351ea79699131b44850b09fec376
meseret1918 <meseret1918@gmail.com>
2024-11-06 13:59:03 +0300

    0x02-react_props

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import $ from 'jquery';
-
-$(document).ready(function () {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
-});
-

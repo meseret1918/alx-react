db487173b2e119f72786d0858faf3d6c1465c3f1
meseret1918 <meseret1918@gmail.com>
2024-11-14 08:58:03 +0300

    task_0

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,25 +1,30 @@
-import { shallow } from "enzyme";
+import { shallow } from "enzyme"; 
 import React from "react";
 import CourseListRow from "./CourseListRow";
 
 describe('Test CourseListRow.js', () => {
-  it('CourseListRow  render without crashing', () => {
-    expect(shallow(<CourseListRow textFirstCell='test' />).exists());
+  it('CourseListRow renders without crashing', () => {
+    expect(shallow(<CourseListRow textFirstCell='test' />).exists()).toBe(true);
   });
 
-  it('render isHeader is True and render with one th', () => {
+  it('renders one <th> with colspan of 2 when isHeader is true and only textFirstCell is provided', () => {
     const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell='test' />);
     expect(wrapper.find('th')).toHaveLength(1);
     expect(wrapper.find('th').prop('colSpan')).toEqual("2");
   });
 
-  it('render isHeader is True and render with two th', () => {
+  it('renders two <th> elements when isHeader is true and both textFirstCell and textSecondCell are provided', () => {
     const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell='test' textSecondCell='test' />);
     expect(wrapper.find('th')).toHaveLength(2);
   });
 
-  it('render isHeader is False and with two td', () => {
+  it('renders two <td> elements when isHeader is false and both textFirstCell and textSecondCell are provided', () => {
     const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell='test' textSecondCell='test' />);
     expect(wrapper.find('td')).toHaveLength(2);
+  });
+
+  it('renders 0 as default for textSecondCell when it is not provided and isHeader is false', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" />);
+    expect(wrapper.find('td').at(1).text()).toEqual("0");  // Verify default to 0
   });
 });
